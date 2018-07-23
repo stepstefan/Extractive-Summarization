a4725815dd5c0d1afd033c1dc97eedcdecfd3a12
anglahel <stefanstepanovic99@hotmail.com>
2018-07-23 10:36:38 +0000

    word features

--- wordfeatures.py
+++ wordfeatures.py
@@ -4,3 +4,58 @@ from nltk.tree import Tree
 from stanfordnlp import StanfordNLP
 
 sNLP = StanfordNLP()
+
+class Wordftrs:
+
+# word, slist - list of sentences in document
+
+    def tf(word, slist):
+        ''' term frequency '''
+        return 0
+
+    def idf(word,slist ):
+        ''' total document number in the datasets, devided by the frequency of documents which contains the word'''
+        return 0
+
+    def cf(word, slist):
+        ''' the frequency of documents which conntains this word in the current cluster'''
+        return 0
+
+    def pos(word):
+        ''' a 4-dimension binary vector indicates whether the word is a noun, a verb, an adjective or an adverb. If the word has another part-of-speech, the vector is all-zero'''
+        return 0
+
+    def namedentity(word):
+        ''' a binary value equals one iff the output of named entity classifier from CoreNLP is not entity'''
+        return 0
+
+    def number(word):
+        ''' a binary value denotes if whether the word is a number'''
+        return 0
+    
+    def slen(word, slist):
+        '''The maximal length of sentences owning the word'''
+        return 0
+
+    def stf(word, slist):
+        '''The maximal TF score of sentences owning the word'''
+        return 0
+
+    def scf(word, slist):
+        '''The maximal CF score of sentences owning the word'''
+        return 0
+
+    def sidf(word, slist):
+        '''The maximal IDF score of sentences owning the word'''
+        return 0
+    def ssubs(word, slist):
+        '''The maximal sub-sentence count of sentences owning the word. A sub-sentence means the node label is S or @S in parsing tree'''
+        return 0
+    def sdepth(word, slist):
+        '''The maximal parsing tree depth of sentences owning the word'''
+        return 0
+
+
+
+
+
