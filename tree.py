6404ca7b14c56c54dc3f0e53f540d618d7eceba7
anglahel <stefanstepanovic99@hotmail.com>
2018-07-25 15:26:46 +0000

    update

---
+++ tree.py
@@ -0,0 +1,12 @@
+import numpy as np
+
+
+class Node:
+    '''Node of the tree'''
+    def __init__(self):
+        self.left = None
+        self.right = None
+        self.parent = None
+        self.salience = 0
+        self.feature = np.array([])
+        
