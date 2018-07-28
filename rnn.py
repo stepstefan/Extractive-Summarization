d827a6e90a2d93edeb43fef37932da314f8e0116
anglahel <stefanstepanovic99@hotmail.com>
2018-07-28 15:29:06 +0000

    2002 features

---
+++ rnn.py
@@ -0,0 +1,17 @@
+import tensorflow as tf
+import numpy as np
+import os
+import matplotlib as plt
+from tree import *
+
+class Config():
+
+
+class RNN():
+
+    def load_data(self):
+        """Loads training data in correct format"""
+
+
+    def 
+
